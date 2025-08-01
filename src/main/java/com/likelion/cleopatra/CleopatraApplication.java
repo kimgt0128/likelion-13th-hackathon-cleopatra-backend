38e3772c7556d89e8209f9caedd0dc757c48887e
KKT <kimgt2828@gmail.com>
2025-08-01 16:20:01 +0900

    initial setting

---
+++ src/main/java/com/likelion/cleopatra/CleopatraApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.cleopatra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleopatraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleopatraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/cleopatra/CleopatraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.cleopatra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleopatraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
