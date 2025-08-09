09c2820eb87ae250d3a6a7743023f7f9543f2d2f
KKT <kimgt2828@gmail.com>
2025-08-09 21:04:55 +0900

    Refact/5-data-collect: Refactor NaverApiService to use Dto - 요청의 응답 형태를 String -> DTO로 변경

--- src/main/java/com/likelion/cleopatra/domain/service/NaverApiService.java
+++ src/main/java/com/likelion/cleopatra/domain/service/NaverApiService.java
@@ -1,5 +1,6 @@
 package com.likelion.cleopatra.domain.service;
 
+import com.likelion.cleopatra.domain.dto.NaverBlogSearchRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -20,7 +21,7 @@ public class NaverApiService {
      * 네이버 블로그 검색 API 비동기 호출
      *
      * @param query 검색어 (예: "공릉 치킨")
-     * @return Mono<String> API 호출 결과를 감싸는 Mono 비동기 스트림
+     * @return Mono<NaverBlogSearchRes> API 호출 결과를 감싸는 Mono 비동기 스트림
      *
      * URI 예시:
      * https://openapi.naver.com/v1/search/blog?query=공릉 치킨
@@ -29,13 +30,13 @@ public class NaverApiService {
      * - .json 확장자는 공식 문서상 생략 권장
      * - 쿼리 파라미터에 띄어쓰기 등 특수문자는 내부에서 자동 인코딩 처리됨
      */
-    public Mono<String> searchBlog(String query) {
+    public  Mono<NaverBlogSearchRes> searchBlog(String query) {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/v1/search/blog.json")
                         .queryParam("query", query)
                         .build())
                 .retrieve()
-                .bodyToMono(String.class);
+                .bodyToMono(NaverBlogSearchRes.class);
     }
 }
