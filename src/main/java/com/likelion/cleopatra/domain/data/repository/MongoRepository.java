ba0bfaf7f2cc3f2af4834f801dcad847cecce4d3
KKT <kimgt2828@gmail.com>
2025-08-11 16:28:50 +0900

    Refact/5-data-collect: MongoDB 테스트 코드 삭제 - PingDoc.java, MongoSmokeRunner.java 삭제

--- src/main/java/com/likelion/cleopatra/domain/data/repository/MongoRepository.java
+++ src/main/java/com/likelion/cleopatra/domain/data/repository/MongoRepository.java
@@ -1,6 +1,4 @@
 package com.likelion.cleopatra.domain.data.repository;
 
-import com.likelion.cleopatra.domain.data.document.PingDoc;
-
 public interface MongoRepository extends org.springframework.data.mongodb.repository.MongoRepository<PingDoc, String> {
 }
