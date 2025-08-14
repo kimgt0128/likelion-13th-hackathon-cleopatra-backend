e4b1996293b28e367a6b345f23c14a463f799603
KKT <kimgt2828@gmail.com>
2025-08-14 14:17:37 +0900

    Feat/14-data-crawl: 크롤링 예외 처리 추가
    - CrawlErrorCode.java: 크롤링 관련 에러 코드 정의
    - CrawlException.java: 크롤링 예외 클래스 정의
    
    입력 검증 / 네트워크 탐색/ 안티봇 차단 / 추출 파싱 / 동시성 중복 / 저장 / 기타로 구성

---
+++ src/main/java/com/likelion/cleopatra/domain/crwal/exception/CrawlException.java
@@ -0,0 +1,7 @@
+package com.likelion.cleopatra.domain.crwal.exception;
+
+public class CrawlException extends RuntimeException {
+    public CrawlException(String message) {
+        super(message);
+    }
+}
